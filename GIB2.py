147b1d622cb70710c4374251eca0139030a0e88c
Mons Erling Mathiesen <mons.e.mathiesen@gmail.com>
2019-02-10 13:42:41 +0100

    Flask tutorial, homepage, aboutpage, navbar

--- GIB2.py
+++ GIB2.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+events = [
+    {
+        'name': 'SLAM POESI',
+        'place': 'Litteraturhuset',
+        'description': 'sykt opplegg'
+    },
+    {
+        'name': 'HalvingFest',
+        'place': 'NardoKlubbhus',
+        'description': 'Var gøy'
+    }
+]
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/home')
+def home():
+    return render_template('home.html', events=events)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title ='About')
 
 
 if __name__ == '__main__':
