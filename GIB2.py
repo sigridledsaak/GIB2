79524e422116e185c698bb433643af3aada4f704
Sirikam <ben.dik@live.no>
2019-02-12 13:33:41 +0100

    the beginning of the database setup

--- GIB2.py
+++ GIB2.py
@@ -1,6 +1,20 @@
 from flask import Flask
+from models import db
 
 app = Flask(__name__)
+
+POSTGRES = {
+    'user': 'postgres',
+    'pw': 'password',
+    'db': 'PostgrSQL 11',
+    'host': 'localhost',
+    'port': '5432',
+}
+
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
+%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+db.init_app(app)
 
 
 @app.route('/')
