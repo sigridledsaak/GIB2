d65bf702161d5eb69ef4393afce9be91f013e3d5
Mons Erling Mathiesen <mons.e.mathiesen@gmail.com>
2019-02-15 09:51:39 +0100

    leaflet inclusion

---
+++ templates/leaflet.js
@@ -0,0 +1,20 @@
+/*
+ the script mus be loaded after the map div is defined.
+ otherwise this will not work (we would need a listener to
+ wait for the DOM to be fully loaded).
+ Just put the script tag below the map div.
+ The source code below is the example from the leaflet start page.
+ */
+
+var map = L.map('map').setView([63.1, 10.4], 13);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoibW9uc2VtIiwiYSI6ImNqczVzdGdmbTAwY24zeW9hMjJtYjk0YnIifQ.ddvzRzPgfKeLtF9RrFuZOg'
+}).addTo(mymap);
+
+L.marker([63.1, 10.4]).addTo(map)
+		.bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+		.openPopup();
