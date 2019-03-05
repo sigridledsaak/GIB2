a6124601a5c6e2eb28608384957d772c92cddcff
Mons Erling Mathiesen <mons.e.mathiesen@gmail.com>
2019-03-05 15:59:21 +0100

    Form on homepage

---
+++ app/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf  import FlaskForm
+from wtforms import StringField, IntegerField, DateField
+from wtforms.validators import DataRequired
+
+class EventForm(FlaskForm):
+    ID = IntegerField('ID', )
+    title = StringField('tittel', validators=[DataRequired()])
+    description = StringField('description')
+    startDate = DateField('startDate')
