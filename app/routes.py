8d8a5f8a0bfaab4d52afa61f17bec7b4e151c5b6
Jakob <sterrip@gmail.com>
2019-03-12 14:59:51 +0100

    registering testevents OK

--- app/routes.py
+++ app/routes.py
@@ -13,18 +13,32 @@ def index():
 @app.route('/')
 @app.route('/home')
 def home():
-
-
+    lng = 10
+    lat = 20
+    venueCoordinates = 'Point(' + str(lng) + ' ' + str(lat) + ')'
     testEvent = Event(titel='firstEvent', startTime='00:00:00.0000000', beskrivelse='dette er det første eventet',startDate='0001-01-01' ,ageLimit=20, endDate='0001-01-01',
-                      isRepetition=False, venueName='Samf', venueCoordinates ='INSERT INTO Event.venueCoordinates() VALUES (Point(42.555, 32.222))', venueAddress='gate', venueID=0,
+                      isRepetition=False, venueName='Samf', venueCoordinates = venueCoordinates, venueAddress='gate', venueID=0,
+                      organizerID=0, organizerName=0, organizerWebsite='hei.no', regularTicketPrize=200,
+                      reducedTicketPrize=100, category_id=0, category_name='fest', pictureURL=0, ticketsURL=0,
+                      moreInfoURL=0, facebookURL=0, videosURL=0)
+
+    testEvent2 = Event(titel='secondEvent', startTime='00:00:00.0000000', beskrivelse='dette er det første eventet',
+                      startDate='0001-01-01', ageLimit=20, endDate='0001-01-01',
+                      isRepetition=False, venueName='Samf', venueCoordinates=venueCoordinates, venueAddress='gate',
+                      venueID=0,
                       organizerID=0, organizerName=0, organizerWebsite='hei.no', regularTicketPrize=200,
                       reducedTicketPrize=100, category_id=0, category_name='fest', pictureURL=0, ticketsURL=0,
                       moreInfoURL=0, facebookURL=0, videosURL=0)
 
     db.session().add(testEvent)
+    db.session().add(testEvent2)
     db.session().commit()
     events = Event.query.all()
     print(events)
+
+    for event in events:
+        print (event.venueCoordinates)
+
 
 
 
@@ -40,3 +54,5 @@ def about():
 @app.route('/register')
 def register():
     return render_template('register.html',title='Register Event')
+
+
