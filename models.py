32247307a007ab58d70bae63dd373433b2498f36
Sirikam <ben.dik@live.no>
2019-03-12 15:53:12 +0100

    adds almost complete xml reading

--- models.py
+++ models.py
@@ -1,67 +1,64 @@
-from GIB2 import db
 from geoalchemy2.types import Geography
 
+from GIB2 import db
 
-class exempleEvent(db.Model):
+
+class Event(db.Model):
     __tablename__ = 'Event'
 
-    ID = db.Column(db.Integer, primary_key=True)
-    titel = db.Column(db.String)
-    beskrivelse = db.Column(db.String)
-    startDate = db.Column(db.DATE)
-    startTime = db.Column(db.TIME)
-    endDate = db.Column(db.DATE)
+    ID = db.Column(db.Integer, primary_key=True, index=True)
+    FID = db.Column(db.Integer, default='none', index=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    startdate = db.Column(db.TIMESTAMP)
+    enddate = db.Column(db.TIMESTAMP)
     isRepetition = db.Column(db.BOOLEAN)
     venueName = db.Column(db.String)
     venueCoordinates = db.Column(Geography(geometry_type='POINT'))
     venueAddress = db.Column(db.String)
-    venueID = db.Column(db.Integer)
-    organizerID = db.Column(db.Integer)
-    organizerName = db.Column(db.String)
+    venueId = db.Column(db.Integer)
+    organizer = db.Column(db.String)
     organizerWebsite = db.Column(db.String)
-    ageLimit = db.Column(db.Integer)
-    regularTicketPrice = db.Column(db.Float)
-    reducedTicketPrice = db.Column(db.Float)
+    ageRestriction = db.Column(db.Integer)
+    regularPrice = db.Column(db.Float)
+    reducedPrice = db.Column(db.Float)
     category_id = db.Column(db.Integer)
     category_name = db.Column(db.String)
-    pictureURL = db.Column(db.String)
+    picture = db.Column(db.String)
     listPictureURL = db.Column(db.String)
     ticketsURL = db.Column(db.String)
     moreInfoURL = db.Column(db.String)
-    facebookURL = db.Column(db.String)
-    videosURL = db.Column(db.String)
+    facebookEventUrl = db.Column(db.String)
+    videoUrl = db.Column(db.String)
 
-
-    def __init__(self, titel, beskrivelse, startDate, startTime, endDate,
+    def __init__(self, FID, title, link, description, startdate, enddate,
                  isRepetition, venueName, venueCoordinates, venueAddress,
-                 venueID, organizerID, organizerName, organizerWebsite,
-                 ageLimit, regularTicketPrize, reducedTicketPrize, category_id,
-                 category_name, pictureURL, ticketsURL, moreInfoURL, facebookURL, videosURL):
-
-        self.titel = titel
-        self.beskrivelse = beskrivelse
-        self.startDate = startDate
-        self.startTime = startTime
-        self.endDate = endDate
+                 venueId, organizer,
+                 organizerName, organizerWebsite, ageRestriction, regularPrize, reducedPrize, category_id,
+                 category_name, picture, ticketsURL, moreInfoURL, facebookEventUrl, videoUrl):
+        self.FID = FID
+        self.title = title
+        self.description = description
+        self.startdate = startdate
+        self.enddate = enddate
         self.isRepetition = isRepetition
         self.venueName = venueName,
         self.venueCoordinates = venueCoordinates
         self.venueAddress = venueAddress
-        self.venueID = venueID
-        self.organizerID = organizerID
+        self.venueId = venueId
+        self.organizer = organizer
         self.organizerName = organizerName
         self.organizerWebsite = organizerWebsite
-        self.ageLimit = ageLimit
-        self.regularTicketPrice = regularTicketPrize
-        self.reducedTicketPrice = reducedTicketPrize
+        self.ageRestriction = ageRestriction
+        self.regularPrice = regularPrize
+        self.reducedPrice = reducedPrize
         self.category_id = category_id
         self.category_name = category_name
-        self.pictureURL = pictureURL
+        self.picture = picture
         self.ticketsURL = ticketsURL
         self.moreInfoURL = moreInfoURL
-        self.facebookURL = facebookURL
-        self.videosURL = videosURL
-
+        self.facebookEventUrl = facebookEventUrl
+        self.videoUrl = videoUrl
 
     def __repr__(self):
         return '<ID {}>'.format(self.ID)
@@ -69,27 +66,28 @@ class exempleEvent(db.Model):
     def serialize(self):
         return {
             'ID': self.ID,
-            'titel': self.titel,
-            'beskrivelse': self.beskrivelse,
-            'startDate': self.startDate,
+            'FID': self.FID,
+            'title': self.title,
+            'description': self.description,
+            'startdate': self.startdate,
             'startTime': self.startTime,
-            'endDate': self.endDate,
+            'enddate': self.enddate,
             'isRepetition': self.isRepetition,
             'venueName': self.venueName,
             'venueCoordinates': self.venueCoordinates,
             'venueAddress': self.venueAddress,
-            'venueID': self.venueID,
-            'organizerID': self.organizerID,
+            'venueId': self.venueId,
+            'organizer': self.organizer,
             'organizerName': self.organizerName,
             'organizerWebsite': self.organizerWebsite,
-            'ageLimit': self.ageLimit,
-            'regularTicketPrize': self.regularTicketPrice,
-            'reducedTicketPrize': self.reducedTicketPrice,
+            'ageRestriction': self.ageRestriction,
+            'regularPrize': self.regularPrice,
+            'reducedPrize': self.reducedPrice,
             'category_id': self.category_id,
             'category_name': self.category_name,
-            'pictureURL': self.pictureURL,
+            'picture': self.picture,
             'ticketsURL': self.ticketsURL,
             'moreInfoURL': self.moreInfoURL,
-            'facebookURL': self.facebookURL,
-            'videosURL': self.videosURL
+            'facebookEventUrl': self.facebookEventUrl,
+            'videoUrl': self.videoUrl
         }
