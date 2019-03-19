81c98783ea3f8d5cbce974a98f37de3b6c612a3c
Mons Erling Mathiesen <mons.e.mathiesen@gmail.com>
2019-03-19 10:01:53 +0100

    Datetime parsing

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+import datetime as dt
 from flask import render_template, request
 from app import app
 from .models import Event
@@ -11,18 +12,27 @@ def index():
 @app.route('/')
 @app.route('/home', methods=['GET', 'POST'])
 def home():
-    date = request.form.get('date')
-    category = request.form.get('category')
-    ageLimit = request.form.get('ageLimit')
-    distance = request.form.get('distance')
-    events = Event.query.filter_by(startdate='date')
-    print(date)
+    if request.method == 'POST':
+        date = dt.datetime.strptime(request.form.get('date'), "%Y-%m-%d").date()
+        print(type(date))
+        print(date)
+        time = dt.datetime.strptime(request.form.get('time'), "")
+        print(type(time))
+        print(time)
+        datetime = dt.datetime.combine(date, time)
+        print(datetime)
+        category = request.form.get('category')
+        ageLimit = request.form.get('ageLimit')
+        distance = request.form.get('distance')
+        events = Event.query.filter_by(startdate=datetime).all()
 
-    #if ageLimit:
-        #events = events.remove(Event.query.filter_by(ageLimit='ageLimit').all())
-    #if category:
-        #events = events.remove(Event.query.filter_by(category='category').all())
-
+        """
+        if ageLimit:
+            events = events.remove(Event.query.filter_by(ageRestriction='ageLimit').all())
+        if category:
+            events = events.remove(Event.query.filter_by(category='category').all())
+        """
+        return render_template('home.html', categories= Event.CATEGORY_CHOICES, events=events)
     return render_template('home.html', categories= Event.CATEGORY_CHOICES)
 
 #@app.route('/home/search', methods=['GET', 'POST'])
