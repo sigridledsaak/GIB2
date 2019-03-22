86be6f60ce1c94195c701a84377cc560379f01f4
Sirikam <ben.dik@live.no>
2019-03-22 11:25:41 +0100

    resetting migrations for the database

--- migrations/env.py
+++ migrations/env.py
@@ -27,20 +27,6 @@ target_metadata = current_app.extensions['migrate'].db.metadata
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
-def exclude_tables_from_config(config_):
-    tables_ = config_.get("tables", None)
-    if tables_ is not None:
-        tables = tables_.split(",")
-    return tables
-
-exclude_tables = exclude_tables_from_config(config.get_section('alembic:exclude'))
-
-def include_object(object, name, type_, reflected, compare_to):
-    if type_ == "table" and name in exclude_tables:
-        return False
-    else:
-        return True
-
 
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
@@ -55,7 +41,7 @@ def run_migrations_offline():
 
     """
     url = config.get_main_option("sqlalchemy.url")
-    context.configure(url=url, include_object=include_object)
+    context.configure(url=url)
 
     with context.begin_transaction():
         context.run_migrations()
